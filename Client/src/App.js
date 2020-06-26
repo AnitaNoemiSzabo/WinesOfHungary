bdbd0a7ee80c084f9ae3f2c0a1e2243807cf70ff
AnitaNoemiSzabo <64478268+Anitaki@users.noreply.github.com>
2020-06-26 19:34:00 +0100

    backend

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,74 +1,105 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 
 export default class App extends Component {
 
-  constructor(props) {
+ constructor(props) {
     super(props);
     this.state = {
-      wine_name: "",   
-      wine_region: "",
-      wine_category: "",
-      winery: "",
-      grape: "",
-      price: "",
+
+      wineShow: false,
+      // wine_name: "",   
+      // wine_region: "winelist.wine_region",
+      // wine_category: "",
+      // winery: "",
+      // grape: "",
+      // price: "",
+      // Image: "",
       winelist: []    //JUST AN ARRAY FOR THE WINELIST. THE OTHER NO NEED
     };
   }
 
-
- 
     componentDidMount() {
-      this.getWinelist();
+      this.getRegionlist();
     }
+
   
-    getWinelist= () => {
-      fetch(`/winelist`)
-        .then(response => response.json())
-        .then(response => {
-          this.setState({ winelist: response });
-        });
-    };
+  getWinelist= () => {
+    fetch(`/winelist`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ winelist: response });
+      });
+  };
+  
+//TO CONNECT ONCLICK TO THIS FUNCTION
+  getWinelistByRegion= (id) => {
+    fetch(`/regionlist/${id}`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ winelist: response });
+      });
+  };
+ 
+
+//name can be different
+  handleClick(e){
+    // e.preventdefault();
+    this.setState({
+      wineShow: !this.state.wineShow
+    });           
+  }
   
 
+// renderFeturedItem(){
+//   return this.props.winelist.map(winelist =>{
+//     return (
+//       <div className="detailedInfo" key = {winelist.wine_name} onClick={() =>{handleClick(winelist)}}>
+//         <p>Wine region: {winelist.wine_region} </p>
+//         <p> Winery: {winelist.winery} </p>
+//         <p>Grape: {winelist.grape}</p>
+//         <p>Type: {winelist.wine_category}  </p>
+//         <p> Price: EUR {winelist.price}.00</p>
+//       </div>
+//           );
+//         });
+//       }
 
 
-    render() {
-
-      return (
-        <div className="App">
-          <h1>Wine List</h1>
-          <div>
-            <h3>List of Wines</h3>
-            <ul>
+  render() {
+    return (
+      <div className="App">
+        <h1>Wine List</h1>
+        <div>
+          <h3>List of Wines</h3>
+          <br/>
+          <ul>
             {this.state.winelist.map((winelist,id) => {
-              return (
-
-              
-              <li>
-                {winelist.wine_name}
-
-
+            return (
+              <li key ={id}>
+              <span onClick={() => this.getWinelist(winelist.id)}>
+              <div>
+                <img onClick={(e) => this.handleClick()} src={winelist.Image} width="150px" height = "100px" alt = {winelist.wine_name}/>
+                <h4 onClick={(e) => this.handleClick()} >{winelist.wine_name} </h4>
+              </div>
+              <div className={this.state.wineShow ? "wineShow" : "wineHidden"}>
+                <p>Wine region: {winelist.wine_region} </p>
+                <p> Winery: {winelist.winery} </p>
+                <p>Grape: {winelist.grape}</p>
+                <p>Type: {winelist.wine_category}  </p>
+                <p> Price: EUR {winelist.price}.00</p>
+                <br/>
+              </div>
+              </span>
               </li>
-            );
-          }
-            )};
-
-            </ul>
-            {/* <ul>
-              {this.state.winelist.map((winelist,id) => {
-                  <li key={id}>
-                <span onClick={() => this.getWinelist(winelist.id)}> 
-                  {winelist.wine_name} {winelist.wine_region} {winelist.wine_category} 
-                  {winelist.winery} {winelist.grape} {winelist.price} 
-                </span> 
-              </li>      
-               }
-              )}
-            </ul>          */}
-          </div>
+              );
+            }
+          )}
+          </ul>
         </div>
-      );
-    }
-  
+      </div>
+    );
+  }
 }
+
+
--- routes/winelist.js
+++ routes/winelist.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
 
+//GET FROM REGIONLIST
+//GET BY REGIONID
 function getItems(req, res) {
   db("SELECT * FROM winelist ORDER BY id ASC;")
     .then(results => {
@@ -20,6 +22,15 @@ router.get("/", function(req, res, next) {
     .catch(err => res.status(500).send(err));
 });
 
+router.get("/regionlist/", function(req, res, next) {
+  
+  db("SELECT * FROM regionlist;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
 
 router.get("/:id", function(req, res, next) {
   db(`SELECT * FROM winelist WHERE id = ${req.params.id};`)
@@ -28,6 +39,16 @@ router.get("/:id", function(req, res, next) {
     })
     .catch(err => res.status(500).send(err));
 });
+
+//TO DIFFERENCIATE FROM OTHER LIST
+router.get("/regionlist/:id", function(req, res, next) {
+  db(`SELECT * FROM regionlist WHERE id = ${req.params.id};`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
 
 
 module.exports = router;
